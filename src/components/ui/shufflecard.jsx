749ecc20194806a5ca3723f66fd439cbed4c9556
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-09-16 22:03:58 +0200

    Add fonts

--- src/components/ui/shufflecard.jsx
+++ src/components/ui/shufflecard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Shufflecard = () => {
   return (
-    <div className='rounded-2xl'>
-
+    <div className='rounded-2xl w-full p-4'>
+        <h1 className='font-oswald text-5xl'>Your new tshirt</h1>
     </div>
   )
 }
