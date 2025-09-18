f04bf0ad4bcd356210c0a6ffa8dbce3d822b94db
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-09-18 17:33:04 +0200

    Add liked heart animation

--- src/components/ui/likedheart.jsx
+++ src/components/ui/likedheart.jsx
@@ -2,19 +2,23 @@ import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
+import { motion } from 'motion/react'
 
 const LikedHeart = () => {
 
     const [isLiked, setIsLiked] = useState(false)
 
   return (
-    <button onClick={() => setIsLiked(!isLiked)} className='cursor-pointer z-10'>
+    <motion.button 
+    animate={isLiked ? {y: [5, -5, 0], scale: [1, 1.1, 1]} : {y: 0} }
+    transition={{duration: 0.2}}
+    onClick={() => setIsLiked(!isLiked)} className='cursor-pointer z-10'>
         {isLiked ? (
             <FontAwesomeIcon color='red' icon={solidHeart}/> 
         ) : (
             <FontAwesomeIcon icon={faHeart}/>
         )}
-    </button>
+    </motion.button>
   )
 }
 
