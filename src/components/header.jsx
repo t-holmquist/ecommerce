a5c3c10d5233db6c610b268c418901ebb2351c7d
Tjalfe Holmquist <tjalfeholmquist@gmail.com>
2025-09-19 10:34:21 +0200

    Add cart item counter animation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import SearchBar from './ui/searchbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'motion/react'
 
 
 const Header = ({setCartIsVisible, cartProducts}) => {
@@ -11,7 +12,10 @@ const Header = ({setCartIsVisible, cartProducts}) => {
           <FontAwesomeIcon size='lg' icon={faShoppingCart}/>
           {/* Cart item counter. If there is more than one item then display counter */}
           {cartProducts.length > 0 && (
-            <div className='absolute flex items-center justify-center top-0 text-sm right-0 bg-red-500 text-white font-source-sans rounded-full w-5 h-5'>{cartProducts.length}</div>
+            <motion.div 
+            initial={{y: 4}}
+            animate={{y: 0}}
+            className='absolute flex items-center justify-center top-0 text-sm right-0 bg-red-500 text-white font-source-sans rounded-full w-5 h-5'>{cartProducts.length}</motion.div>
           )}
         </button>
     </div>
